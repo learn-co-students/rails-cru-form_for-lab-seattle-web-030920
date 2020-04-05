71f7e9d30ce1781f06fc7f758482d989f0ec7d22
github username <github email address>
2020-04-05 15:07:04 -0700

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,34 @@
+class ArtistsController < ApplicationController
+    def show
+        @artist = find_artist(params)
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(artist_params)
+        redirect_to artist_path(@artist)
+    end
+
+    def edit
+        @artist = find_artist(params)
+    end
+
+    def update
+        @artist = find_artist(params)
+        @artist.update(artist_params)
+        redirect_to artist_path(@artist)
+    end
+
+    private
+
+    def find_artist(params)
+        Artist.find(params[:id])
+    end
+
+    def artist_params
+        params.require(:artist).permit(:name, :bio)
+    end
+end
